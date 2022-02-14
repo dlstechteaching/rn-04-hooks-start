a5dc7dff4af7a0b9def83b2d608194704553b991
dls.tech.teaching <dls.tech.teaching@gmail.com>
2022-02-14 22:54:58 +0100

    removing commented code

--- src/navigation/Navigation.ts
+++ src/navigation/Navigation.ts
@@ -1,7 +1,4 @@
 export enum Navigation {
     Home = 'app.home',
-    Playground = 'app.playground',
-    PlaygroundUseEffect = 'app.playground.useeffect',
-    // PlaygroundUseState = 'app.playground.usestate',
-    // PlaygroundUseState = 'app.playground.usestate',
+    Playground = 'app.playground'
 }
